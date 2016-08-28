1583726d815ffce14f08d40010f2161621fc857c
iyaozhen <pop59461@126.com>
2016-08-29 00:24:46 +0800

    add queue

--- filebeat.py
+++ filebeat.py
@@ -5,8 +5,8 @@
 
 Authors: iyaozhen
 Date: 2016-04-20
-Since: v2.0
-多文件配置支持
+Since: v2.0 2016-8-28
+多日志支持
 """
 
 import socket
@@ -28,21 +28,19 @@ class FileBeat(object):
     py filebeat 类
     """
     @classmethod
-    def publish_to_logstash(cls, sockets, data, fields=None, timeout=10):
+    def publish_to_logstash(cls, sockets, data, timeout=10):
         """
-        发布数据到logstash
-        随机选取下游节点
-        发出去的是个json格式数据包, 方便设置一些自定义字段, logtash接收数据时配置:
-        input {
-            tcp {
-                port => xxxx
-                codec => json
-            }
-        }
+        发布数据到logstash集群
         Args:
             sockets: 已经和logstash建立的长连接dict
             data: 需要推送的数据
-            fields: 自定义字段
+                发出去的是个json格式数据包, 可以很方便设置一些自定义字段, logtash接收数据时配置:
+                input {
+                    tcp {
+                        port => xxxx
+                        codec => json
+                    }
+                }
             timeout: 某个logtash节点挂掉超时等待时间
 
         Returns:
@@ -52,38 +50,27 @@ class FileBeat(object):
         if sockets is False:
             return False
         else:
-            # 数据封装
-            packet_data = {
-                'message': data.encode('utf8', 'ignore')
-            }
-            # 添加自定义字段
-            if fields is not None:
-                for key, value in fields.iteritems():
-                    packet_data[key] = value
-            packet_data = json.dumps(packet_data) + '\r\n'
-
-            # 随机选取出一个有效的socket通道
+            # 随机选取出一个有效的socket通道（负载均衡）
             random_address = cls.__random_choice_socket(sockets)
             try:
                 if random_address is False:
-                    # 没有可用通道, 手动抛出异常, 保证捕获异常后有一次重连机会
+                    # 没有可用通道, 手动抛出异常
                     raise socket.error
                 else:
-                    res = sockets[random_address].sendall(packet_data)
+                    res = sockets[random_address].sendall(data + '\r\n')
             except socket.error:
                 if random_address is not False:
                     # 将出错的socket置为False
                     sockets[random_address] = False
-                # 等待, 然后尝试重连
+                # 尝试重连
                 time.sleep(timeout)
                 cls.re_connect(sockets)
-                # 所有的通道都已经挂了则直接返回发送失败(本次发布操作不再进行重试), 防止进程间管道阻塞
-                # 因此logstash集群挂掉期间的数据会丢失
-                if cls.is_all_fail(sockets):
+                # 重新连接集群失败，本次数据丢失
+                if cls.sockets_all_fail(sockets):
                     return False
                 else:
                     # 尝试重新发送
-                    return cls.publish_to_logstash(sockets, data, fields)
+                    return cls.publish_to_logstash(sockets, data, timeout)
             else:
                 return res
 
@@ -121,7 +108,7 @@ class FileBeat(object):
         for address in addresses:
             sockets[address] = cls.get_socket(address)
         # 建立连接都没有成功
-        if cls.is_all_fail(sockets):
+        if cls.sockets_all_fail(sockets):
             return False
         else:
             return sockets
@@ -141,7 +128,7 @@ class FileBeat(object):
                 sockets[address] = cls.get_socket(address)
 
     @staticmethod
-    def is_all_fail(sockets):
+    def sockets_all_fail(sockets):
         """
         检查已经建立连接的sockets是否都挂了
         Args:
@@ -158,6 +145,14 @@ class FileBeat(object):
 
     @staticmethod
     def __random_choice_socket(sockets):
+        """
+        随机选择一个可用的通道
+        Args:
+            sockets: sockets dict
+
+        Returns:
+            socket object, if all failure return False
+        """
         real_sockets = {}
         for address, socket in sockets.iteritems():
             if socket is not False:
@@ -165,6 +160,21 @@ class FileBeat(object):
 
         if real_sockets:
             return random.choice(real_sockets.keys())
+        else:
+            return False
+
+    @staticmethod
+    def __random_trigger(denominator):
+        """
+        1/denominator return True
+        Args:
+            denominator: 概率分母
+
+        Returns:
+            bool
+        """
+        if random.randint(1, denominator) == 1:
+            return True
         else:
             return False
 
@@ -340,78 +350,104 @@ class FileBeat(object):
         handler.setFormatter(formatter)
         logger.addHandler(handler)
 
+    @classmethod
+    def prospector(cls, prospector_conf, queue):
+        file_path = prospector_conf['path']
+        file_date_ext = prospector_conf['date_ext']
+        encoding = prospector_conf['encoding']
+        include_lines = prospector_conf['include_lines']
+        exclude_lines = prospector_conf['exclude_lines']
+        fields = prospector_conf['fields']
 
-def worker(sockets, prospector, queue):
-    file_path = prospector['path']
-    file_date_ext = prospector['date_ext']
-    encoding = prospector['encoding']
-    include_lines = prospector['include_lines']
-    exclude_lines = prospector['exclude_lines']
-    fields = prospector['fields']
-
-    current_file_path = FileBeat.get_current_path(file_path, file_date_ext)
-    last_file_path = current_file_path
-    # 如果文件不存在, 等待当前文件生成
-    logging.info("waiting file %s create" % current_file_path)
-    while FileBeat.is_non_zero_file(current_file_path) is False:
-        time.sleep(60)
         current_file_path = FileBeat.get_current_path(file_path, file_date_ext)
-    while True:
-        # 创建子进程tail文件
-        logging.info("start tail file " + current_file_path)
-        (process, poll) = FileBeat.tail_file(current_file_path, True)
-        if process is False:
-            error_str = poll
-            logging.error(error_str)
-        # 轮训子进程是否获取到数据
+        last_file_path = current_file_path
+        # 如果文件不存在, 等待当前文件生成
+        logging.info("waiting file create %s" % current_file_path)
+        while FileBeat.is_non_zero_file(current_file_path) is False:
+            time.sleep(60)
+            current_file_path = FileBeat.get_current_path(file_path, file_date_ext)
         while True:
-            if poll.poll(1):  # timeout 1s
-                data = process.stdout.readline().rstrip()
-                # 统一转换为unicode编码
-                data_unicode = data.decode(encoding, 'ignore')
-                queue.put(data_unicode)
-                # if FileBeat.data_filter(data_unicode, include_lines, exclude_lines):
-                #     if FileBeat.publish_to_logstash(sockets, data_unicode, fields) is False:
-                #         logging.error("publish to logstash fail [%s]" % data)
-                #     else:
-                #         logging.info("publish to logstash success")
-            else:
-                # 若当前目标日志文件名变化, 则跳出循环, 读取新的文件
-                current_file_path = FileBeat.get_current_path(file_path, file_date_ext)
-                if current_file_path != last_file_path:
-                    poll.unregister(process.stdout)
-                    process.kill()
-                    last_file_path = current_file_path
-                    break
+            # 创建子进程tail文件
+            logging.info("start tail file %s" + current_file_path)
+            (process, poll) = FileBeat.tail_file(current_file_path, True)
+            if process is False:
+                error_str = poll
+                logging.error(error_str)
+            # 轮训子进程是否获取到数据
+            while True:
+                if poll.poll(1):  # timeout 1s
+                    data = process.stdout.readline().rstrip()
+                    logging.debug("get data form subprocess.PIPE [%s]", data)
+                    # 统一转换为unicode编码
+                    data_unicode = data.decode(encoding, 'ignore')
+                    if FileBeat.data_filter(data_unicode, include_lines, exclude_lines):
+                        # 数据封装
+                        packaged_data = {
+                            'message': data_unicode.encode('utf8', 'ignore')
+                        }
+                        # 添加自定义字段
+                        if fields is not None:
+                            for key, value in fields.iteritems():
+                                packaged_data[key] = value
+                        packaged_data = json.dumps(packaged_data)
+                        logging.debug("packaged data [%s]", data)
+                        # put no wait
+                        try:
+                            queue.put(packaged_data, False)
+                        except Queue.Full:
+                            logging.error("queue is full, data loss [%s]" % packaged_data)
+                else:
+                    # 若当前目标日志文件名变化, 则跳出循环, 读取新的文件
+                    current_file_path = FileBeat.get_current_path(file_path, file_date_ext)
+                    if current_file_path != last_file_path:
+                        poll.unregister(process.stdout)
+                        process.kill()
+                        last_file_path = current_file_path
+                        break
 
 
-def run():
+def main():
     """
-    运行任务
+    主进程，读取配置文件然后创建子进程运行日志收集任务
     Returns:
-
+        None
     """
-    FileBeat.init_log("./filebeat", logging.ERROR)
+    FileBeat.init_log("./filebeat", logging.DEBUG)
 
     try:
         conf_file = sys.argv[1]
     except IndexError:
-        logging.warning("use default configure file: filebeat.json")
+        logging.info("use default configure file filebeat.json")
         conf_file = "filebeat.json"
 
     with open(conf_file, 'r') as f:
         conf = json.load(f)
 
     prospectors = conf['prospectors']
+    q_maxsize= conf['filebeat']['queue_maxsize']
     logstash_hosts = conf['logstash']['hosts']
 
     sockets = FileBeat.get_sockets(logstash_hosts)
     if sockets is False:
-        sys.exit("error: can not connect logstash clusters")
+        con_fail = "[error] can not connect logstash clusters"
+        logging.error(con_fail)
+        sys.exit(con_fail)
 
-    q = Queue.Queue(maxsize=9999)
-    for prospector in prospectors:
-        multiprocessing.Process(target=worker, args=(sockets, prospector, q))
+    # 和子进程间使用队列通讯
+    q = Queue.Queue(maxsize=q_maxsize)
+    for prospector_conf in prospectors:
+        multiprocessing.Process(target=FileBeat.prospector, args=(prospector_conf, q))
+
+    while True:
+        # block get data from the queue.
+        data = q.get()
+        if FileBeat.publish_to_logstash(sockets, data) is False:
+            logging.error("publish to logstash failure [%s]" % data)
+        else:
+            logging.info("publish to logstash success")
+
+    logging.info('exit')
+
 
 if __name__ == "__main__":
-    run()
+    main()
